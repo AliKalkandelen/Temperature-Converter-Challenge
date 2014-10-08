ab96d319abc1d253e0058bb684161e2cb97da66a
ALI KALKANDELEN <ALIKAL18@GMAIL.COM>
2014-10-08 10:39:44 -0400

    update main

--- main.cpp
+++ main.cpp
@@ -1,44 +1,52 @@
 #include <iostream>
-#include <math.h>
-#include <stdio.h>
-#include <cctype>
+#include <stdlib.h>
+#include <limits>
 using namespace std;
 
 
 
+int main() {
 
-int main()
+double lowerLimit=0;
+double higherLimit=0;
+double step=0;
+
+cout<<"Please give in a lower limit, limit >= 0: 10\n\n";
+cin>>lowerLimit;
+while(!(cin >> lowerLimit) || lowerLimit < 0 || lowerLimit > 500)
 {
-int lowerLimit;
-int higherLimit;
-int step;
+    cout << "invalid input!\n";
+    cin.clear();
+    cin.ignore( numeric_limits<int>::max(), '\n' );
+}
 
-
-do
+cout<<"Please give in a higher limit, 10 > limit <= 500: 20 \n\n";
+cin>>higherLimit;
+while(!(cin >> higherLimit) || higherLimit <= 10 || higherLimit > 500)
 {
-    cout<<"Please give in a lower limit, limit >= 0: 10\n\n";
-   cin>>lowerLimit;
-    if(!isdigit(lowerLimit) || lowerLimit < 0)
-        cout<<"Invalid Entry\n\n";
-
-}while(!isdigit(lowerLimit) || lowerLimit < 0);
-
-do
+    cout << "invalid input!\n";
+    cin.clear();
+    cin.ignore( numeric_limits<int>::max(), '\n' );
+}
+cout<<"Please give in a step. 0 < step <= 10: 4\n\n";
+cin>>step;
+while(!(cin >> step) || step <= 0 || step >10)
 {
-    cout<<"Please give in a higher limit, 10 > limit <= 500: 20 \n\n";
-    cin>>higherLimit;
-    if(!isdigit(higherLimit) == false || higherLimit < 10 || higherLimit > 500)
-        cout<<"Invalid Entry\n\n";
-}while(!isdigit(higherLimit) == false || higherLimit < 10 || higherLimit > 500);
+    cout << "invalid input!\n";
+    cin.clear();
+    cin.ignore( numeric_limits<int>::max(), '\n' );
+}
+
+cout<<lowerLimit<<higherLimit<<step<<"\n\n";
+cout<<"Thanks!";
 
 
-do
-{
-    cout<<"Please give in a step. 0 < step <= 10: 4";
-    cin>>step;
-    if(!isdigit(step) == false || step < 1 || step > 4)
-        cout<<"Invalid Entry\n\n";
-}while (!isdigit(step) == false || step < 1 || step > 4);
+
+
+
+
+
+
 
 
 
